4bf10836c11c8e19df16192002327a97bc372d50
Seymour-creates <romelogilbert@yahoo.com>
2021-07-17 11:24:37 -0400

    deleted react

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
   entry: [
     'react-hot-loader/patch',
-    './src/index.js'
+    './dist/index.js'
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
