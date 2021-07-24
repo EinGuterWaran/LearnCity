cb91f95f31bfa193f5cf37d00744f977b4372bc6
Janusan Lingeswaran <janusan.lingeswaran@rwth-aachen.de>
2021-07-24 16:24:48 +0200

    Created a Landing Page + restructured everything

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded',(event) => {
-   const pTag =  document.createElement('p');
-   pTag.innerText = 'Hello World'
-   const body = document.getElementById('app');
-   body.appendChild(pTag);
-   console.log(event)
-})
