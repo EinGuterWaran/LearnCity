b64cf3de2c18e8df7087df17aa9dae13977cf276
Philipp Kant <ptckant@gmail.com>
2021-07-31 00:00:48 +0200

    aparment more appealing

--- storage_handler.js
+++ storage_handler.js
@@ -10,7 +10,7 @@ async function getOriginalJSON(){
     var actURL = window.location.href;
     var path = window.location.pathname;
     var actURL = actURL.replace(path,'');
-    var userData = await getJson(actURL+'/LearnCity/data.json');
+    var userData = await getJson(actURL+'/data.json');
     return userData;
 }
 
